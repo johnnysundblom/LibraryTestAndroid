d79a22fbc4093ddda88a900f1d4d5a74146758b6
Johnny Sundblom <johnnysundblom@Johnnys-MacBook-Pro.local>
2019-01-17 09:05:22 +0100

    updates

---
+++ jslibrary/src/main/cpp/native-lib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Johnny Sundblom on 2019-01-16.
+//
+
+#include "native-lib.h"
---
+++ jslibrary/src/main/cpp/native-lib.h
@@ -0,0 +1,8 @@
+//
+// Created by Johnny Sundblom on 2019-01-16.
+//
+
+#ifndef LIBRARYTEST_NATIVE_LIB_H
+#define LIBRARYTEST_NATIVE_LIB_H
+
+#endif //LIBRARYTEST_NATIVE_LIB_H
